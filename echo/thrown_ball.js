2a4509b4ae81eb13787b4a974262c3b4aad91408
rachel-sedibridge <rachelbridge.sedivy@gmail.com>
2025-11-24 17:22:33 -0500

    implement the duration-only (jeff's ball throwing analogy) depth map idea

--- echo/thrown_ball.js
+++ echo/thrown_ball.js
@@ -1,12 +1,9 @@
-// JEFF'S VARIATION ON ECHO: SONIFY TRAJECTORY OF A PROJECTILE (BASEBALL)
+// JEFF'S VARIATION ON ECHO: SONIFY TRAJECTORY OF BALL HITTING OBJECTS
 
 /*
 IDEA:
 Like the echo idea, but the "echo" has no additional processing, and
 serves only to reinforce how long the initial tone is.
-
-USEFUL NOTES:
-
 
 LIMITATIONS:
 - not configurable from the web page (need to edit code)
@@ -28,8 +25,8 @@ const schema_url = "json_schemas/city_street.json"
 const TOGGLE_PLAY = ' '; //key to toggle play/pause
 
 // sonification timing parameters
-const TONE_SPACING = 0.5; //in seconds, shouldn't be less than max delay
-const ECHO_DURATION = 0.7; //in seconds, how long the echoes last
+const TONE_SPACING = 0.5; //in seconds, time between end of one tone and start of next
+const STOP_DURATION = 0.3; //in seconds, how long the "stop" tone lasts
 
 var toneEvents = []; //list of tone event objs used in playback, populate during loading
 
@@ -51,45 +48,35 @@ function generateTonesFromObjects(data) {
     var objName = `${obj.type}${obj.ID}`;
     var x = obj.centroid[0];
     var depth = obj.depth;
+  
     // create the main tone and the panned "stop" tone
     // NOTE: both are the same sound for all objs, multiple instances for unique panning
-    var objTone = new Tone.Sampler({
-      urls: {
-          D1: D_URL, //not actually octave 1 but doesn't matter for this
-      },
-      baseUrl: "audio_tracks/",
-      release: 0.3,
-    });
-    // stop tone doesn't need to be Sampler, but easier if they're the same
+    var objTone = new Tone.Sampler(
+      { D1: D_URL }, //not actually octave 1 but doesn't matter for this
+      {
+        baseUrl: "audio_tracks/",
+        release: 0.3,
+      }
+    );
+    // stop tone doesn't need to be Sampler, but easier if they're the same.
+    // its pitch name is totally meaningless, just use same one as object tone
     var stopTone = new Tone.Sampler({
-      urls: {
-          D1: "audio_tracks/tennis_ball_hit.mp3",
-      },
-      release: 0.2,
+        D1: "audio_tracks/tennis_ball_hit.mp3",
     });
     // set 2D pan using x coordinate of centroid
     var panner = new Tone.Panner(normalizePanX(x));
-    // find the time between the intial tone and the echo
-    var duration = normalizeDepthToTime(depth)
-    // create effects for the echo
-    var r_decay, r_wet = normalizeDepthToReverb(depth);
-    var reverb = new Tone.Reverb({
-      decay: r_decay,
-      wet: r_wet
-    });
-    var lowPassFilter = new Tone.Filter({
-      type: "lowpass",
-      frequency: normalizeDepthToFilter(depth)
-    });
+    // the "stop" tone I picked is pretty loud, bring it down to pull focus away
+    var volume = new Tone.Volume(-14)
     // apply those effects to the echo, and the panning to both
     objTone.chain(panner, Tone.Destination);
-    stopTone.chain(reverb, lowPassFilter, panner, Tone.Destination)
-
+    stopTone.chain(volume, panner, Tone.Destination);
+  
     // save essential info in an event array that can be passed to Part or Sequence
     toneEvents.push({
       name: objName,
-      tone: objTone,
-      duration: duration,
+      objTone: objTone,
+      stopTone: stopTone,
+      duration: normalizeDepthToTime(depth),
       time: 0 //set later! as in, literally the next thing...
     });
   }
@@ -101,51 +88,37 @@ function normalizePanX(x) {
   return -1 + 2 * x;
 }
 
-// get echo delay time in seconds, from obj depth num [0,1]
+// get duration of object tone in seconds, from obj depth num [0,1]
 function normalizeDepthToTime(depth) {
   // [0,1] -> [c,d] : f(t) = c + (d-c/1-0) * (t - 0)
-  var delay_min = 0.01; //seconds when depth = 0
+  var delay_min = 0.3; //seconds when depth = 0
   var delay_max = 3; //seconds when depth = 1
-  return delay_min + (delay_max - delay_min) * depth;
+  return delay_min + (delay_max - delay_min) * depth; //linear
 }
 
-// get the params for reverb (`decay`, `wet`) from obj depth num [0,1]
-function normalizeDepthToReverb(depth) {
-  var decay_min = 0.5; //seconds, when depth = 0
-  var decay_max = 5; //seconds, when depth = 1
-  var decay = decay_min + (decay_max - decay_min) * depth;
-
-  var wet_min = 0.5; //wet-dry balance when depth = 0
-  var wet_max = 0.96; //wet-dry balance when depth = 1
-  var wet = wet_min + (wet_max - wet_min) * depth;
-
-  return decay, wet;
-}
-
-// get the cutoff frequency of the low pass filter from obj depth num [0,1]
-function normalizeDepthToFilter(depth) {
-  // I went on desmos until I got a func that looked the right shape... see docs
-  var freq_min = 950; //Hz when depth = 0
-  var freq = freq_min + Math.pow((-9 * depth + 9.5), 3.5);
-  // ^sets max cutoff ~3600, but want no discernible EQ on extremely close objs
-  if (depth > 0.95) {
-    freq = 6000
-  }
-  return freq;
-}
+// LEAVING THIS IN CASE IT BECOMES USEFUL
+// // get volume reduction of "stop" tone in decibels, from depth [0,1]
+// // NOTE: this is not meant to be meaningful, but rather to blend the "stop"
+// //       tone into the end of the object tone as it decays, and avoid confusion
+// // NOTE: the volume is reduced at baseline bc the stock tone is pretty loud
+// function normalizeDepthToVolume(depth) {
+//   var reduce_min = -9; //decibel change when depth = 0 (min)
+//   var reduce_max = -14; //decibel change when depth = 1 (max)
+//   return reduce_min + (reduce_max - reduce_min) * depth; //linear
+// }
 
 // set the start times for each tone in the toneEvents array
 // increase next start time by (this one + its length + echo duration + spacing)
 function setStartTimes() {
   var curTime = 0;
   for (var i = 0; i < toneEvents.length; i++) {
-    toneEvents[i].time = curTime;
-    curTime = curTime + toneEvents[i].echoDelay + ECHO_DURATION + TONE_SPACING;
+    toneEvents[i].time = curTime; //0 when i = 0
+    curTime = curTime + toneEvents[i].duration + STOP_DURATION + TONE_SPACING;
   }
 }
 
 
-// KEYBINDING
+// KEYBINDINGS / PLAYBACK
 document.addEventListener('keydown', handleDown);
 // no listener for keyup, since only working w/ single keypresses
 
@@ -160,8 +133,8 @@ function handleDown(e) {
     // TODO: make it stop playback once it's started
   }
   else {
-    // playAllTones();
-    playbackTester();
+    playAllTones();
+    // playbackTester();
   }
 }
 
@@ -175,9 +148,8 @@ function playAllTones() {
 // args MUST be (time, value) (API requirement)
 function playTone(time, value) {
   // value contains `name` for the 'captioning', not implemented yet
-  var duration = value.echoDelay < 0.4 ? 0.4 : value.echoDelay;
-  value.tone.triggerAttackRelease("D1", duration, time);
-  value.echo.triggerAttackRelease("D1", ECHO_DURATION, time + value.echoDelay);
+  value.objTone.triggerAttackRelease("D1", value.duration, time);
+  value.stopTone.triggerAttackRelease("D1", STOP_DURATION, time + value.duration);
 }
 
 
@@ -191,35 +163,35 @@ function playbackTester() {
     pan: 1
   });
 
-  basicTone.chain(channel1, Tone.getDestination());
-  basicTone.triggerAttackRelease("D1", 0.5, 0);
-  // basicTone.disconnect(channel1);
-  // basicTone.toDestination()
+  // basicTone.chain(channel1, Tone.getDestination());
+  // basicTone.triggerAttackRelease("D1", 0.5, 0);
+  // // basicTone.disconnect(channel1);
+  // // basicTone.toDestination()
 
-  basicTone.chain(channel2, Tone.getDestination());
-  basicTone.triggerAttackRelease("D1", 0.5, 1);
-  // basicTone.disconnect(channel2);
+  // basicTone.chain(channel2, Tone.getDestination());
+  // basicTone.triggerAttackRelease("D1", 0.5, 1);
+  // // basicTone.disconnect(channel2);
 }
 
-const basicTone = new Tone.Sampler({
-  urls: {
-      D1: 'audio_tracks/clean_d_str_pick.mp3',
-  },
-  release: 0.3,
-  onload: () => {
-    // console.log('pluck loaded')
-    // basicTone.triggerAttackRelease("D1", 0.6);
-  }
-});
+// const basicTone = new Tone.Sampler({
+//   urls: {
+//       D1: 'audio_tracks/clean_d_str_pick.mp3',
+//   },
+//   release: 0.3,
+//   onload: () => {
+//     // console.log('pluck loaded')
+//     // basicTone.triggerAttackRelease("D1", 0.6);
+//   }
+// });
 
-const sampler = new Tone.Sampler(
-  {
-    A1: "audio_tracks/tennis_ball_hit.mp3",
-    D1: 'audio_tracks/clean_d_str_pick.mp3'
-  }
-).toDestination();
+// const sampler = new Tone.Sampler(
+//   {
+//     A1: "audio_tracks/tennis_ball_hit.mp3",
+//     D1: 'audio_tracks/clean_d_str_pick.mp3'
+//   }
+// ).toDestination();
 
-document.querySelector("button").addEventListener("click", () => {
-  sampler.triggerAttackRelease("A1", 0.5, 1);
-  sampler.triggerAttackRelease("D1", 1, 0);
-});
+// document.querySelector("button").addEventListener("click", () => {
+//   sampler.triggerAttackRelease("A1", 0.5, 1);
+//   sampler.triggerAttackRelease("D1", 1, 0);
+// });
